21f5e54573cdb2c40b9d431e116be8831d14f70f
harry2992 <blackcigar2992@gmail.com>
2025-04-08 01:13:53 -0400

    Split the architecture into GMF, MLP and NCF

--- ncf.py
+++ ncf.py
@@ -1,7 +1,111 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import time
+
+class MLP(nn.Module):
+    def __init__(
+            self,
+            num_users,
+            num_items,
+            embedding_dim=32,
+            feature_dims=None,
+            dropout=0.0,
+            layers_ratio=2,
+            num_layers=4,
+            gaussian_mean=0,
+            gaussian_std=0.01
+    ):
+        super(MLP, self).__init__()
+
+        self.user_embedding = nn.Embedding(num_users, embedding_dim)
+        self.item_embedding = nn.Embedding(num_items, embedding_dim)
+        mlp_input_dim = embedding_dim * 2
+
+        self.projection_layers = nn.ModuleDict()
+        if feature_dims is not None:
+            for feature, (input_dim, output_dim) in feature_dims.items():
+                if input_dim == 1:
+                    self.projection_layers[feature] = nn.Linear(input_dim, output_dim)
+                else:
+                    self.projection_layers[feature] = nn.EmbeddingBag(input_dim, output_dim, mode='mean')
+                mlp_input_dim += output_dim
+
+        self.dropout = dropout
+        self.layers = nn.ModuleList()
+
+        for i in range(0, num_layers):
+            mlp_output_dim = int(mlp_input_dim/layers_ratio)
+            self.layers.append(nn.Linear(mlp_input_dim, mlp_output_dim))
+            mlp_input_dim = mlp_output_dim
+
+        self._init_weights(gaussian_mean, gaussian_std)
+
+    def _init_weights(self, mean=0, std=0.01):
+        """
+        Initialize all weights using Normal distribution
+        Args:
+            mean: Mean of the Gaussian distribution
+            std: Standard deviation of the Gaussian distribution
+        """
+        nn.init.normal_(self.user_embedding.weight, mean=mean, std=std)
+        nn.init.normal_(self.item_embedding.weight, mean=mean, std=std)
+
+        # if self.projection_layers is not None:
+        #     for layer in self.projection_layers:
+        #         if isinstance(layer, nn.Embedding):
+        #             nn.init.normal_(layer.weight, mean=mean, std=std)
+
+    def forward(self, user, item, features):
+        user_embed = self.user_embedding(user)
+        item_embed = self.item_embedding(item)
+        vector = torch.cat([user_embed, item_embed], dim=-1)
+
+        for feature, layer in self.projection_layers.items():
+            if isinstance(layer, nn.Linear):
+                feature_embed = layer(features[feature].unsqueeze(-1))
+            else:
+                indices, offsets = features[feature]
+                feature_embed = layer(indices, offsets)
+            vector = torch.cat([vector, feature_embed], dim=-1)
+
+        for layer in self.layers:
+            vector = F.relu(layer(vector))
+            if self.dropout > 0:
+                vector = F.dropout(vector, p=self.dropout, training=self.training)
+
+        return vector
+
+class GMF(nn.Module):
+    def __init__(
+        self,
+        num_users,
+        num_items,
+        factors=32,
+        gaussian_mean=0,
+        gaussian_std=0.01
+    ):
+        super(GMF, self).__init__()
+
+        self.user_embedding = nn.Embedding(num_users, factors)
+        self.item_embedding = nn.Embedding(num_items, factors)
+
+        self._init_weights(gaussian_mean, gaussian_std)
+
+    def _init_weights(self, mean=0, std=0.01):
+        """
+        Initialize all weights using Normal distribution
+        Args:
+            mean: Mean of the Gaussian distribution
+            std: Standard deviation of the Gaussian distribution
+        """
+        nn.init.normal_(self.user_embedding.weight, mean=mean, std=std)
+        nn.init.normal_(self.item_embedding.weight, mean=mean, std=std)
+
+    def forward(self, user, item):
+        user_embed = self.user_embedding(user)
+        item_embed = self.item_embedding(item)
+        vector = torch.mul(user_embed, item_embed)
+        return vector
 
 class NCF(nn.Module):
     def __init__(
@@ -10,7 +114,7 @@ class NCF(nn.Module):
         num_items,
         factors=8,
         mlp_user_item_dim=32,
-        mlp_additional_features=None, # Dictionary with keys are features, values are tuples of (input, output)
+        mlp_feature_dims=None, # Dictionary with keys are features, values are tuples of (input, output)
         num_mlp_layers=4,
         layers_ratio=2,
         dropout=0,
@@ -18,80 +122,29 @@ class NCF(nn.Module):
         gaussian_std=0.01
     ):
         super(NCF, self).__init__()
+
+        self.gmf_module = GMF(num_users, num_items, factors, gaussian_mean, gaussian_std)
         
-        # Embedding layers
-        self.mf_user_embedding = nn.Embedding(num_users, factors)
-        self.mf_item_embedding = nn.Embedding(num_items, factors)
-        
-        self.mlp_user_embedding = nn.Embedding(num_users, mlp_user_item_dim)
-        self.mlp_item_embedding = nn.Embedding(num_items, mlp_user_item_dim)
-        mlp_input_size = mlp_user_item_dim * 2
+        self.mlp_module = MLP(
+            num_users=num_users,
+            num_items=num_items,
+            embedding_dim=mlp_user_item_dim,
+            feature_dims=mlp_feature_dims,
+            dropout=dropout,
+            layers_ratio=layers_ratio,
+            num_layers=num_mlp_layers,
+            gaussian_mean=gaussian_mean,
+            gaussian_std=gaussian_std,
+        )
+        mlp_output_dim = self.mlp_module.layers[num_mlp_layers-1].out_features
 
-        self.projection_layers = nn.ModuleDict()
-        if mlp_additional_features is not None:
-            for feature, (input_dim, output_dim) in mlp_additional_features.items():
-                if input_dim == 1:
-                    self.projection_layers[feature] = nn.Linear(input_dim, output_dim)
-                else:
-                    self.projection_layers[feature] = nn.EmbeddingBag(input_dim, output_dim, mode='mean')
-                mlp_input_size += output_dim
+        self.prediction = nn.Linear(factors + mlp_output_dim, 1)
 
-        # MLP layers
-        self.dropout = dropout
-        self.mlp_layers = nn.ModuleList()
+    def forward(self, user, item, features):
+        gmf_vector = self.gmf_module(user, item)
+        mlp_vector = self.mlp_module(user, item, features)
 
-        mlp_output_size = 0
-        for i in range(0, num_mlp_layers):
-            mlp_output_size = int(mlp_input_size/layers_ratio)
-            self.mlp_layers.append(nn.Linear(mlp_input_size, mlp_output_size))
-            mlp_input_size = mlp_output_size
-        
-        self.prediction = nn.Linear(factors + mlp_output_size, 1)
-
-        # Initialize weights using Gaussian distribution
-        self._init_weights(gaussian_mean, gaussian_std)
-
-    def _init_weights(self, mean=0, std=0.01):
-        """
-        Initialize all weights using Gaussian distribution
-        Args:
-            mean: Mean of the Gaussian distribution
-            std: Standard deviation of the Gaussian distribution
-        """
-        # Initialize embedding layers
-        nn.init.normal_(self.mf_user_embedding.weight, mean=mean, std=std)
-        nn.init.normal_(self.mf_item_embedding.weight, mean=mean, std=std)
-        nn.init.normal_(self.mlp_user_embedding.weight, mean=mean, std=std)
-        nn.init.normal_(self.mlp_item_embedding.weight, mean=mean, std=std)
-
-        # if self.projection_layers is not None:
-        #     for layer in self.projection_layers:
-        #         if isinstance(layer, nn.Embedding):
-        #             nn.init.normal_(layer.weight, mean=mean, std=std)
-
-    def forward(self, user, item, additional_features):
-        mf_user_embed = self.mf_user_embedding(user)
-        mf_item_embed = self.mf_item_embedding(item)
-        mf_vector = torch.mul(mf_user_embed, mf_item_embed)
-
-        mlp_user_embed = self.mlp_user_embedding(user)
-        mlp_item_embed = self.mlp_item_embedding(item)
-        mlp_vector = torch.cat([mlp_user_embed, mlp_item_embed], dim=-1)
-
-        for feature, layer in self.projection_layers.items():
-            if isinstance(layer, nn.Linear):
-                feature_embed = layer(additional_features[feature].unsqueeze(-1))
-            else:
-                indices, offsets = additional_features[feature]
-                feature_embed = layer(indices, offsets)
-            mlp_vector = torch.cat([mlp_vector, feature_embed], dim=-1)
-
-        for layer in self.mlp_layers:
-            mlp_vector = F.relu(layer(mlp_vector))
-            if self.dropout > 0:
-                mlp_vector = F.dropout(mlp_vector, p=self.dropout, training=self.training)
-
-        vector = torch.cat([mf_vector, mlp_vector], dim=-1)
+        vector = torch.cat([gmf_vector, mlp_vector], dim=-1)
         logit = self.prediction(vector)
         output = torch.sigmoid(logit)
 
